7cf7fe3e077d046f2f811d6e461e35ee53c0b661
s4archivosxx <jordan_vasquez_2019@hotmail.com>
2025-07-09 13:13:04 -0500

    reto completado

--- Python/generador-de-emails/generador_email.py
+++ Python/generador-de-emails/generador_email.py
@@ -1,1 +1,14 @@
-# Mensaje de bienvenida 2.
+# Mensaje de bienvenida 1.
+print("*** Bienvenido al sistema de generación email de cuidad gotica ***")
+
+# Solicitar nombre y obtener el nombre de la consola
+name = input("Cual es tu nombre: ")
+name = name.lower()
+# Solicitar apellido y obtener el apellido de la consola
+lastname = input("Cual es tu apellido: ")
+lastname = lastname.lower()
+
+# Mensaje final
+print(" Tu nuevo email generador por el sistema es: ")
+print(f"\t{name}.{lastname}@ciudadgotica.com")
+print("\t *** Felicidades ***")
